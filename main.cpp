58e4b83e2209431fa1a54b271886d2ff4d646acc
DarkTong <13427508433@163.com>
2017-04-23 20:41:26 +0800

    纹理

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cstdio>
+#include <cmath>
 
 // GLEW
 #define GLEW_STATIC
@@ -7,6 +7,12 @@
 
 // GLFW
 #include <GLFW/glfw3.h>
+
+// SOIL
+#include <SOIL.h>
+
+//myShader
+#include "Shader.h"
 
 using namespace std;
 
@@ -19,6 +25,9 @@ double red=0.1, green=0.3, bule=0.3;
 // 定点数量
 GLint numberOfVertex=12;
 
+// 两图片的显示比例
+GLfloat value;
+
 GLfloat vertices1[]=
 {
     0.5f, 0.0f, 0.0f,   //中右
@@ -26,55 +35,28 @@ GLfloat vertices1[]=
     -0.5f, -0.5f, 0.0f, //左下
     0.0f, -0.5f, 0.0f
 };
-
-// 三角形定点坐标
+// triangle & color
 GLfloat vertices[]=
 {
-    // top
-    -0.5, 0.5f, 0.0f,   //左上
-    0.0f, 0.5f, 0.0f,   //中上
-    0.5f, 0.5f, 0.0f,   //右上
-    // middle
-    -0.5f, 0.0f, 0.0f,  //中左
-    0.0f, 0.0f, 0.0f,   //中中
-    0.5f, 0.0f, 0.0f,   //中右
-    // buttom
-    -0.5f, -0.5f, 0.0f, //左下
-    0.0f, -0.5f, 0.0f,  //中下
-    0.5f, -0.5f, 0.0f   //右下
+    //position          //color             //texture position
+    0.5f, 0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+    0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+    -0.5f, 0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f
+    //0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+    //-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+    //0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 1.0f
 };
-
 GLuint indices[]=
 {
-    0, 1, 3,    //第一个三角形
-    1, 2, 5,    //第二个三角形
-    3, 6, 7,
-    5, 7, 8
+    0, 1, 2,    //第一个三角形
+    0, 2, 3,    //第二个三角形
+    //3, 6, 7,
+    //5, 7, 8
 };
-
-// 定点着色器源码
-const GLchar* vertexShaderSource="#version 330 core\n"
-    "layout (location = 0) in vec3 position;\n"
-    "void main(){\n"
-    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-    "}\n\0";
-// 片段着色器源码
-const GLchar* fragmentShaderSource="#version 330 core\n"
-    "out vec4 color;\n"
-    "void main(){\n"
-    "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-const GLchar* fragmentShaderSource1="#version 330 core\n"
-    "out vec4 color;\n"
-    "void main(){\n"
-    "color = vec4(0.8f, 0.8f, 0.8f, 1.0f);\n"
-    "}\n\0";
-
 // Function prototypes
 void key_callback(GLFWwindow *, GLint,  GLint, GLint, GLint);
 void changeRGB();
-void shaderCompileInfo(GLuint&);
-void shaderLinkInfo(GLuint&);
 int main()
 {
 
@@ -124,47 +106,7 @@ int main()
     glViewport(0, 0, width, height);
 
     /* 1.创建顶点着色器和线段着色器 */
-    // 编译定点着色器
-    GLuint vertexShader, vertexShader1;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // -2
-    vertexShader1 = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader1, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader1);
-    // 检测是否编译错误
-    shaderCompileInfo(vertexShader);
-    shaderCompileInfo(vertexShader1);
-
-    // 编译片段着色器
-    GLuint fragmentShader, fragmentShader1;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // -2
-    fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader1, 1, &fragmentShaderSource1, NULL);
-    glCompileShader(fragmentShader1);
-    // 检测是否编译错误
-    shaderCompileInfo(fragmentShader);
-    shaderCompileInfo(fragmentShader1);
-
-    /* 2.构建着色器程序 */
-    // 链接着色器的程序
-    GLuint shaderProgram, shaderProgram1;
-    shaderProgram = glCreateProgram();
-    // 添加着色器（按顺序）(由于原理是利用前一个输出作为后一个的输入）；
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // -2
-
-    shaderProgram1 = glCreateProgram();
-    glAttachShader(shaderProgram1, vertexShader1);
-    glAttachShader(shaderProgram1, fragmentShader1);
-    glLinkProgram(shaderProgram1);
-
+    Shader shaderProgram("./vertexAndColorShader.vs", "./fragmentShader.fs");
 
     /* 3.创建顶点缓存对象,顶点数组对象 */
     GLuint VBO, VBO1; //Vertex Buffer Objects
@@ -182,33 +124,60 @@ int main()
     glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices, indices, GL_STATIC_DRAW);
     // 3.4设置顶点指针属性
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0); //第一个参数对应 layout的location
     glEnableVertexAttribArray(0);
+    // 3.4.1设置颜色属性
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+    // 3.4.2设置纹理属性
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6*sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
     // 3.4解绑VAO
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    //申请
-    glGenBuffers(1, &VBO1);
-    glGenVertexArrays(1, &VAO1);
-    //绑定
-    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
-    glBindVertexArray(VAO1);
-    //写进BVBO1
-    glBufferData(GL_ARRAY_BUFFER, sizeof vertices1, vertices1, GL_STATIC_DRAW);
-    //设置定点指针属性
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-    //解绑
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    /* 纹理 */
+    // 1.利用SOUL加载图片
+    int width1, height1;
+    unsigned char* image = SOIL_load_image("container.jpg", &width1, &height1, 0, SOIL_LOAD_RGB);
+    std::cout << "sizeof jpg:" << sizeof(image) << std::endl;
+    // 2.获取纹理对象
+    GLuint texture;
+    glGenTextures(1, &texture);
+    // 3.绑定纹理
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // 4.生成纹理
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width1, height1, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    // 5.解绑
+    glBindTexture(GL_TEXTURE_2D, 0);
+    // 6.释放图片内存
+    SOIL_free_image_data(image);
+
+    int width2, height2;
+    GLuint texture1;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    unsigned char* image1 = SOIL_load_image("awesomeface.png", &width2, &height2, 0, SOIL_LOAD_RGB);
+    std::cout << "sizeof png:" << sizeof(image1) << std::endl;
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width2, height2, 0, GL_RGB, GL_UNSIGNED_BYTE, image1);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    SOIL_free_image_data(image1);
+    /* 纹理结束 */
 
     /* 画图（渲染） */
     // 为了使画图程序不会在执行完后立刻退出
-        // 检查事件， 检查有没有触发什么时间（键盘输入和鼠标移动）
+    GLfloat shift = 0.0;
+    value = 0.5;
     while(!glfwWindowShouldClose(window)) // 检查GLFW是否被要求退出
     {
+        // 检查事件， 检查有没有触发什么时间（键盘输入和鼠标移动）
         glfwPollEvents();
 
         // 渲染指令， 创建完窗口我们就可以通知GLFW将我们窗口的上下文设置为当前线程的主上下文了。
@@ -217,31 +186,56 @@ int main()
         changeRGB();
 
         // 渲染指令，绘制物体
-        glUseProgram(shaderProgram);    //加载着色器程序
+        shaderProgram.Use();   //加载着色器程序
 
-        /* 一个VBO、VAO
+        // 更新unifoorm颜色
+        //GLfloat timeValue = glfwGetTime();
+        //GLfloat greenValue = (sin(timeValue)/2)+0.5;
+        //GLint vertexColorLocation = glGetUniformLocation(shaderProgram.Program, "ourColor");
+        //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        // 更新uniform颜色
+        //shift += 0.001f;
+        //GLint vertexPositionLocation = glGetUniformLocation(shaderProgram.Program, "shift");
+        //glUniform3f(vertexPositionLocation, shift, 0.0f, 0.00f);
+        // 加载纹理单元到着色器程序
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUniform1i(glGetUniformLocation(shaderProgram.Program, "ourTexture"), 0);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glUniform1i(glGetUniformLocation(shaderProgram.Program, "ourTexture1"), 1);
+        glUniform1f(glGetUniformLocation(shaderProgram.Program, "value"), value);
+
         // 绑定
         glBindVertexArray(VAO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  //      glBindTexture(GL_TEXTURE_2D, texture);
 
         // 画图
         //someOpenGLFunctionDrawsOurTriangle();
         //glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 1, 3);
         //glDrawArrays(GL_TRIANGLES, 3, 3);
         glDrawElements(GL_TRIANGLES, numberOfVertex, GL_UNSIGNED_INT, 0);
 
         // 解绑
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+//        glBindTexture(GL_TEXTURE_2D, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
-        */
+
+        /*
+        // first triangle
+        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 1, 3);
         glBindVertexArray(0);
+
+        // second triangle
         glUseProgram(shaderProgram1);
         glBindVertexArray(VAO1);
         glDrawArrays(GL_TRIANGLES, 0, 3);
         glBindVertexArray(0);
-
+        */
 
         // 交换缓存，增强视觉效果
         glfwSwapBuffers(window);
@@ -261,44 +255,27 @@ void key_callback(GLFWwindow *window, GLint key,  GLint scancode, GLint action, 
     {
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
+    else if(key == GLFW_KEY_UP && action == GLFW_PRESS)
+    {
+        value += 0.05f;
+        if(value >= 1.0f)
+            value = 1.0f;
+    }
+    else if(key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+    {
+        value -= 0.05;
+        if(value <= 0.0f)
+            value = 0.0f;
+    }
 }
 
 // 渲染操作函数
 void changeRGB()
 {
-    red += 0.001;
-    green += 0.002;
-    bule += 0.0001;
+    red += 0.0001;
+    green += 0.0002;
+    bule += 0.00001;
     if(red > 0.7) red -= 0.6;
     if(green > 0.6) green -= 0.3;
     if(bule > 0.9) bule -= 0.6;
-}
-
-// 检测着色器是否编译成功
-void shaderCompileInfo(GLuint& shader)
-{
-    // 检查着色器是否编译错误
-    GLint success;
-    GLchar infoLog[512];
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-
-    // 若变异错误则输出
-    if(!success)
-    {
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "ERROR:COMPILE FAILED\n" << infoLog << std::endl;
-    }
-}
-
-// 检测链接器是否正确链接
-void shaderLinkInfo(GLuint& shaderProgram)
-{
-    GLint success;
-    GLchar infoLog[512];
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR:LINK FAILED\n" << infoLog << std::endl;
-    }
 }
